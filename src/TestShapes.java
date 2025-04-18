bf0b4c80678ad9ce63ed49a6d302123e31c5c6b9
Doğukan Cındık <dogukancindik@posta.mu.edu.tr>
2025-04-18 12:27:03 +0300

    Update TestShapes.java

--- src/TestShapes.java
+++ src/TestShapes.java
@@ -1,10 +1,11 @@
 public class TestShapes {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Rectangle r1 = new Rectangle(color;"blue", sideA;4);
-        Rectangle r2 = new Rectangle(color;"blue", sideA;4);
-        System.out.println(r1.equlas(r2));
+        Shape shape = new Shape();
+        shape.describe();
 
-
+        Rectangle r1 = new Rectangle(color: "blue ",sideA :4 )
+        Rectangle r2 = new Rectangle(color: "blue ",sideA :4 )
+        System.out.println();
     }
 }
